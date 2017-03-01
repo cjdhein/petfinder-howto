cdca4ad233b55269ea210b9a67823231ae2c3e6a
Cody <cdhein2002@yahoo.com>
2017-03-01 16:05:40 -0600

    Add files via upload

--- petfind.js
+++ petfind.js
@@ -3,6 +3,7 @@
  */
 
 var results;
+var favorite_ids = new Map();
 var usable_info = {
     "name"  : "",
     "age"   : "",
@@ -10,31 +11,68 @@ var usable_info = {
     "gender": "",
     "breeds" : [],
     "notes" : [],
+	"photo" : "",
+	"id"	:"",
 }
 
 console.log(usable_info.breeds);
 console.log(usable_info.notes);
 
-var pet_format = '<div class="panel panel-primary">'+
-    '      <div class="panel-heading"><span class="api_input"></span></div>'+
-    '      <div class="panel-body container">'+
-    '        <div class="row pet-info-row">'+
-    '          <p class="col-lg-2">Age: <span class="api_input"></span></p>'+
-    '          <p class="col-lg-2">Size:    <span class="api_input"></span></p>'+
-    '          <p class="col-lg-2">Gender:  <span class="api_input"></span></p>'+
-    '        </div>'+
-    '        <div class="row pet-info-row">'+
-    '          <div class="col-lg-4"><div class="api_input">Breed(s): </div></div>'+
-    '          <div class="col-lg-4">Notes: <span class="api_input"></span></div>'+
-    '        </div>'+
-    '      </div>'+
-    '    </div>';
+var pet_format =  '<div class="panel panel-primary pet_name">'+
+' <div class="panel-heading"><button class="btn btn-xs btn-warning fav-btn"><span class="glyphicon glyphicon-star-empty"</span></button><span class="api_input"></span></div>'+
+' <div class="panel-body container">'+
+'	<div class="row pet-info-row">'+
+'		<img class="pet_image col-lg-2" src=""></img>'+
+'		<div class="row">'+
+'			<p class="col-lg-2">Age: <span class="api_input"></span></p>'+
+'			<p class="col-lg-2">Size:    <span class="api_input"></span></p>'+
+'			<p class="col-lg-2">Gender:  <span class="api_input"></span></p>'+
+'			<div class="row pet-info-row">'+
+'				<div class="col-lg-4"><div class="api_input">Breed(s): </div></div>'+
+'				<div class="col-lg-4">Notes: <span class="api_input"></span></div>'+
+'			</div>   '+
+'		</div>'+
+'	</div>'+
+' </div>'+
+'</div>';
+	
+function logFav(info, favbtn){
+	if(favorite_ids.has(info)){
+		favbtn.target.firstChild.className = "glyphicon glyphicon-star-empty";
+		favorite_ids.delete(info);
+	}else{
+		favbtn.target.firstChild.className = "glyphicon glyphicon-star";
+		favorite_ids.set(info, info);
+	}
+}
 
 $(document).ready(function(){
-    sampleCall();
+    firstCall();
+	$("#getFavs").click(function(event){
+		event.preventDefault();
+		favorite_ids.forEach(function(value, key, map){
+			favCall(value);
+			
+		});
+	});
 });
 
-function sampleCall(){
+function favCall(id){
+	var params = id;
+	var endbit = "&output=basic&format=json&callback=?";
+    var favURL = "https://api.petfinder.com/pet.get?key=dcc5be07eeae2e3fed49e529ed8cf73b&id=";
+    console.log("1");
+    $.ajax({
+        dataType: "jsonp",
+        url: favURL+params+endbit,
+        success:(function(data){
+            var favResult = [data.petfinder.pet];
+            grabInfo(favResult, ($("#favorite-pets")));
+        })
+    });	
+}
+
+function firstCall(){
     var sampleURL = "https://api.petfinder.com/pet.find?animal=dog&key=dcc5be07eeae2e3fed49e529ed8cf73b&location=53217&count=4&output=basic&format=json&callback=?";
     console.log("1");
     $.ajax({
@@ -42,20 +80,20 @@ function sampleCall(){
         url: sampleURL,
         success:(function(data){
             results = data.petfinder.pets.pet;
-            grabInfo(results);
+            grabInfo(results, ($("#example")));
         })
     });
 }
 
-function grabInfo(res){
+function grabInfo(res, whereToDisplay){
 
     for(var q = 0; q < res.length; q++){
-        console.log(i);
         usable_info.name = res[q].name.$t;
         usable_info.age = res[q].age.$t;
         usable_info.size = res[q].size.$t;
         usable_info.gender = res[q].sex.$t;
-
+		usable_info.photo = res[q].media.photos.photo[2].$t;
+		usable_info.id = res[q].id.$t;
 
         var breeds = res[q].breeds.breed;
         for(var i = 0; i < breeds.length; i++){
@@ -68,8 +106,6 @@ function grabInfo(res){
             console.log(notes[i].$t);
             usable_info.notes[i] = notes[i].$t;
         }
-
-
 
         switch (usable_info.size){
             case "S":
@@ -98,37 +134,47 @@ function grabInfo(res){
             default:
                 usable_info.gender = usable_info.gender;
         }
+		
+		var thisInfo = usable_info;
+		
+		displayInfo(thisInfo, whereToDisplay);
 
-        var new_pet = $(pet_format);
-
-        var inputs = new_pet.find('.api_input');
-        $(inputs[0]).text(usable_info.name);
-        $(inputs[1]).text(usable_info.age);
-        $(inputs[2]).text(usable_info.size);
-        $(inputs[3]).text(usable_info.gender);
-        $(inputs[6]).text(usable_info.description);
-
-        for(var i = 0; i < usable_info.breeds.length; i++){
-            var breed_line = $("<li style='margin-left:20px' class='breed_line'></li>");
-            breed_line.text(usable_info.breeds[i]);
-            $(inputs[4]).append(breed_line);
-        }
-
-        for(var f = 0; f < usable_info.notes.length; f++){
-            var notes_line = $("<li style='margin-left:20px' class='notes_line'></li>");
-            notes_line.text(usable_info.notes[f]);
-            $(inputs[5]).append(notes_line);
-            console.log(usable_info.notes[f]);
-        }
-
-        $("#example").append(new_pet);
 
     }
 
 }
 
-function displayInfo(){
+function displayInfo(infoToDisplay, whereToDisplay){
+        var new_pet = $(pet_format);
+		var favbtn = new_pet.find('.fav-btn');
+		favbtn.on('click',function(favbtn){
+			var fav_id =favbtn.delegateTarget.id; 
+			logFav(fav_id, favbtn);
+			
+		});
+		
+        var inputs = new_pet.find('.api_input');
+        $(inputs[0]).text(infoToDisplay.name);
+        $(inputs[1]).text(infoToDisplay.age);
+        $(inputs[2]).text(infoToDisplay.size);
+        $(inputs[3]).text(infoToDisplay.gender);
+        $(inputs[6]).text(infoToDisplay.description);
+		new_pet.find('.fav-btn').attr("id", infoToDisplay.id);
+		new_pet.find('.pet_image').attr("src", infoToDisplay.photo);
+		
+        for(var i = 0; i < infoToDisplay.breeds.length; i++){
+            var breed_line = $("<li style='margin-left:20px' class='breed_line'></li>");
+            breed_line.text(infoToDisplay.breeds[i]);
+            $(inputs[4]).append(breed_line);
+        }
 
+        for(var f = 0; f < infoToDisplay.notes.length; f++){
+            var notes_line = $("<li style='margin-left:20px' class='notes_line'></li>");
+            notes_line.text(infoToDisplay.notes[f]);
+            $(inputs[5]).append(notes_line);
+            console.log(infoToDisplay.notes[f]);
+        }
+        whereToDisplay.append(new_pet);
 }
 
 
